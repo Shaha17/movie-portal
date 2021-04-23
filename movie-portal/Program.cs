5891e6e47a8a2aa8ce30ad24a16216beaeabfcbf
ShahaKB <shahzodjabbarov@gmail.com>
2021-04-23 14:51:12 +0500

    add migration

--- movie-portal/Program.cs
+++ movie-portal/Program.cs
@@ -3,24 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using movie_portal.Context;
+using movie_portal.Models.Account;
 
 namespace movie_portal
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var host = CreateHostBuilder(args).Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+			using var scope = host.Services.CreateScope();
+			var services = scope.ServiceProvider;
+			var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+			try
+			{
+				var context = services.GetRequiredService<MoviePortalContext>();
+				
+				await context.Database.MigrateAsync();
+
+				logger.LogInformation("Migrate successfull");
+			}
+			catch (Exception ex)
+			{
+				logger.LogError(ex.Message);
+			}
+
+			await host.RunAsync();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }
--- movie-portal/Startup.cs
+++ movie-portal/Startup.cs
@@ -2,58 +2,95 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using movie_portal.Context;
+using movie_portal.Models.Account;
 
 namespace movie_portal
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews();
 
-            
-        }
+			services.AddDbContext<MoviePortalContext>(option =>
+			{
+				option.UseSqlServer(Configuration.GetConnectionString("Default")).UseLazyLoadingProxies();
+			});
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			services.AddIdentity<User, IdentityRole>(option =>
+			{
+				option.User.AllowedUserNameCharacters = null;
+			}).AddRoleManager<RoleManager<IdentityRole>>()
+			  .AddUserManager<UserManager<User>>()
+			  .AddEntityFrameworkStores<MoviePortalContext>()
+			  .AddDefaultTokenProviders();
 
-            app.UseRouting();
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
-            app.UseAuthorization();
+			services.AddAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			services.Configure<IdentityOptions>(option =>
+			{
+				option.Password.RequireDigit = false;
+				option.Password.RequireLowercase = false;
+				option.Password.RequireUppercase = false;
+				option.Password.RequireNonAlphanumeric = false;
+				option.Password.RequiredLength = 4;
+				option.User.RequireUniqueEmail = true;
+			});
+
+			services.ConfigureApplicationCookie(option =>
+			{
+				option.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+				option.SlidingExpiration = true;
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
